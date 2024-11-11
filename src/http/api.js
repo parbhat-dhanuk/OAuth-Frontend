aff799cc486a518b0aa160b4cad49fe339ddc24c
parbhat-dhanuk <parbhatdhanuk3@gmail.com>
2024-11-11 17:30:52 +0545

    cors fixed

--- src/http/api.js
+++ src/http/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API= axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://oauth-backend-77re.onrender.com",
     headers:{
         "Content-Type":'application/json'
     }
